ea146652b8b735dc38714a8e17c94f0cd9d482b9
StefanButnaru04 <161626677+StefanButnaru04@users.noreply.github.com>
2024-03-07 00:48:56 +0200

    Add files via upload

---
+++ Laborator 2/prb1/NumberList.h
@@ -0,0 +1,12 @@
+class NumberList
+{
+    int *numbers=new int[10];
+    int count;
+public:
+    void Init();
+    void Add(int x);
+
+    void Sort();
+    void Print();
+    void Dealocare();
+};
